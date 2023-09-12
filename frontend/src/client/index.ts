1c6642e00d4eeb4f64e43f2e5bff2155684fb6aa
Eugene Cheltsov <chill.icp@gmail.com>
2023-09-13 01:02:54 +0900

    frontend: cleanup schemas

--- frontend/src/client/index.ts
+++ frontend/src/client/index.ts
@@ -18,12 +18,4 @@ export { RobotStatus } from "./models/RobotStatus";
 export type { RobotUpdate } from "./models/RobotUpdate";
 export type { ValidationError } from "./models/ValidationError";
 
-export { $HTTPValidationError } from "./schemas/$HTTPValidationError";
-export { $Page_Robot_ } from "./schemas/$Page_Robot_";
-export { $Robot } from "./schemas/$Robot";
-export { $RobotCreate } from "./schemas/$RobotCreate";
-export { $RobotStatus } from "./schemas/$RobotStatus";
-export { $RobotUpdate } from "./schemas/$RobotUpdate";
-export { $ValidationError } from "./schemas/$ValidationError";
-
 export { RobotsService } from "./services/RobotsService";
