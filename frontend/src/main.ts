38ee049d2a6fc5953094a6ad97be79cc6844ab1d
Eugene Cheltsov <chill.icp@gmail.com>
2023-09-13 01:05:50 +0900

    frontend: cleanup pinia

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,6 @@
 import "./assets/main.css";
 
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import { ApiClient } from "./client";
@@ -9,7 +8,6 @@ import { apiClientInjectKey } from "./constants/injection";
 
 const app = createApp(App);
 
-app.use(createPinia());
 app.use(router);
 app.provide(
   apiClientInjectKey,
