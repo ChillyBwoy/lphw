0f5a445fe12d394dd6487726ee6792fb8256fd1b
Eugene Cheltsov <chill.icp@gmail.com>
2023-09-12 11:32:56 +0900

    frontend: add markdown

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MarkdownPreview from "@/components/MarkdownPreview.vue";
+import Readme from "../../README.md";
+</script>
 
 <template>
-  <main>main</main>
+  <main>
+    <MarkdownPreview>
+      <Readme />
+    </MarkdownPreview>
+  </main>
 </template>
